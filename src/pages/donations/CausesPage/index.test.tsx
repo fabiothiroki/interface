c694ec2fa278ce6620274971763cd75217654088
Nicholas Marques <nnmarques97@gmail.com>
2022-01-24 16:06:43 -0300

    chore: update test template with new test helpers

--- src/pages/donations/CausesPage/index.test.tsx
+++ src/pages/donations/CausesPage/index.test.tsx
@@ -1,11 +1,9 @@
 import React from "react";
-import { screen } from "@testing-library/react";
 import { renderComponent } from "config/testUtils";
 import Causes from ".";
 
 describe("Causes", () => {
   it("should render without error", () => {
     renderComponent(<Causes />);
-    expect(screen.getByText("Causes")).toBeInTheDocument();
   });
 });
