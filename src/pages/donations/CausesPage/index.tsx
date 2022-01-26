e638161dd9ac9a112d30c0bf705c8dc7b20e164f
Clara Mendes <claraplanejamento@gmail.com>
2022-01-26 09:26:26 -0300

    feat: adds ngo card

--- src/pages/donations/CausesPage/index.tsx
+++ src/pages/donations/CausesPage/index.tsx
@@ -1,9 +1,18 @@
+import CardCenterImageButton from "components/moleculars/cards/CardCenterImageButton";
+import ngoFactory from "config/testUtils/factories/ngoFactory";
+import { useState } from "react";
+import Ngo from "types/entities/Ngo";
 import * as S from "./styles";
 
 function CausesPage(): JSX.Element {
+  const [ngos, setNgos] = useState<Ngo[]>([ngoFactory()]);
+
   return (
     <S.Container>
       <h1>Causes</h1>
+      {ngos.map((ngo) => (
+        <CardCenterImageButton image={ngo.image} title={ngo.name} buttonText="doar" />)
+      }
     </S.Container>
   );
 }
