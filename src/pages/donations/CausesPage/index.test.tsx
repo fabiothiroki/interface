62581c8c621d54364b6b9d3cf5eb4cb0e3a6f001
Nicholas Marques <nnmarques97@gmail.com>
2022-01-26 16:51:05 -0300

    test: add tests to CausesPage

--- src/pages/donations/CausesPage/index.test.tsx
+++ src/pages/donations/CausesPage/index.test.tsx
@@ -1,9 +1,25 @@
 import React from "react";
-import { renderComponent } from "config/testUtils";
+import { expectTextToBeInTheDocument, renderComponent } from "config/testUtils";
+import Header from "components/atomics/sections/Header";
 import Causes from ".";
 
+// jest.mock("components/atomics/sections/Header", () => jest.fn(() => <div />));
+// const spy = jest.spyOn(Header, "Header");
+
 describe("Causes", () => {
-  it("should render without error", () => {
+  beforeEach(() => {
     renderComponent(<Causes />);
+  });
+
+  it("renders the title", () => {
+    expectTextToBeInTheDocument("Causes");
+  });
+
+  it("renders the subtitle", () => {
+    expectTextToBeInTheDocument("Donate for free for a cause of your choice");
+  });
+
+  xit("renders the header", () => {
+    expect(Header).toHaveBeenCalled();
   });
 });
