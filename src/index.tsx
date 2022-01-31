5bb8b55b3bd9a3c945dbdfa2ee8eaa386c78bb79
Nicholas Marques <nnmarques97@gmail.com>
2022-01-31 11:08:39 -0300

    fix: add absolute import to useContract log error

--- src/index.tsx
+++ src/index.tsx
@@ -14,6 +14,7 @@ declare global {
 window.ethereum = window.ethereum || {};
 initializeFirebase();
 initializeSentry();
+// eslint-disable-next-line no-console
 console.log(process.env);
 
 ReactDOM.render(
