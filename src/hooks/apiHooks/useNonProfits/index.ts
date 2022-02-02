c48194f044fae5a249f867b29c8f3bc8ef1f57c0
Nicholas Marques <nnmarques97@gmail.com>
2022-02-02 13:44:00 -0300

    refactor: update apiHooks folder

--- src/hooks/apiHooks/useNonProfits/index.ts
+++ src/hooks/apiHooks/useNonProfits/index.ts
@@ -2,7 +2,7 @@ import { useApi } from "hooks/useApi";
 import nonProfitsApi from "services/api/nonProfitsApi";
 import NonProfit from "types/entities/NonProfit";
 
-function useGroups() {
+function useNonProfits() {
   const { data: nonProfits, isLoading } = useApi<NonProfit[]>({
     key: "nonProfits",
     fetchMethod: nonProfitsApi.getNonProfits,
@@ -11,4 +11,4 @@ function useGroups() {
   return { nonProfits, isLoading };
 }
 
-export default useGroups;
+export default useNonProfits;
