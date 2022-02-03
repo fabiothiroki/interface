9201e8939d7428937f814e2f1a2db58c3691eb70
Nicholas Marques <nnmarques97@gmail.com>
2022-02-03 09:26:26 -0300

    feat: add refetch to useNonProfits

--- src/hooks/apiHooks/useNonProfits/index.ts
+++ src/hooks/apiHooks/useNonProfits/index.ts
@@ -3,12 +3,16 @@ import nonProfitsApi from "services/api/nonProfitsApi";
 import NonProfit from "types/entities/NonProfit";
 
 function useNonProfits() {
-  const { data: nonProfits, isLoading } = useApi<NonProfit[]>({
+  const {
+    data: nonProfits,
+    isLoading,
+    refetch,
+  } = useApi<NonProfit[]>({
     key: "nonProfits",
     fetchMethod: nonProfitsApi.getNonProfits,
   });
 
-  return { nonProfits, isLoading };
+  return { nonProfits, isLoading, refetch };
 }
 
 export default useNonProfits;
