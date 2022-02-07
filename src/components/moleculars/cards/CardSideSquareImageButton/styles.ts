34f50af974a9dcd969bb7fb491b525b79ac04f8f
Clara Mendes <claraplanejamento@gmail.com>
2022-02-07 08:40:07 -0300

    style: adds media query to cardSideSquareImageBut.

--- src/components/moleculars/cards/CardSideSquareImageButton/styles.ts
+++ src/components/moleculars/cards/CardSideSquareImageButton/styles.ts
@@ -2,10 +2,13 @@ import styled, { css } from "styled-components";
 import Button from "components/atomics/Button";
 
 export const Container = styled.div`
-  ${() => css`
-    max-width: 344px;
-    height: 100%;
-    display: flex;
+  ${({ theme }) => css`
+  height: 100%;
+  display: flex;
+  margin-top: 40px;
+  max-width: 320px;
+  @media (min-width: ${theme.breakpoints.pad}) {
+    max-width: 390px;
   `}
 `;
 
