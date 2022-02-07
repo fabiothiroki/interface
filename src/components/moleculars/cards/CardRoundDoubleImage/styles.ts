8cfd23c4611bb3eb33e37acd29eb22ca7ffeb8fa
Clara Mendes <claraplanejamento@gmail.com>
2022-02-07 08:49:34 -0300

    style: adds media query to cardRoundDoubleImage

--- src/components/moleculars/cards/CardRoundDoubleImage/styles.ts
+++ src/components/moleculars/cards/CardRoundDoubleImage/styles.ts
@@ -2,10 +2,10 @@ import styled, { css } from "styled-components";
 
 export const Container = styled.div`
   ${() => css`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
   `}
 `;
 
@@ -23,22 +23,30 @@ export const ImageContainer = styled.div`
 export const LeftImage = styled.img`
   ${({ theme }) => css`
     position: relative;
-    width: 200px;
-    height: 200px;
+    width: 180px;
+    height: 180px;
     left: 25px;
     border-radius: 50%;
     border: solid 5px ${theme.colors.ribonWhite};
     filter: drop-shadow(0px 20px 40px ${theme.colors.ribonShadow});
+    @media (min-width: ${theme.breakpoints.pad}) {
+      width: 200px;
+      height: 200px;
+    }
   `}
 `;
 
 export const RightImage = styled.img`
   ${({ theme }) => css`
     position: relative;
-    width: 200px;
-    height: 200px;
+    width: 180px;
+    height: 180px;
     right: 25px;
     border-radius: 50%;
     filter: drop-shadow(0px 20px 40px ${theme.colors.ribonShadow});
+    @media (min-width: ${theme.breakpoints.pad}) {
+      width: 200px;
+      height: 200px;
+    }
   `}
 `;
