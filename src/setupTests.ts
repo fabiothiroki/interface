bb647e19ca9cbf5470f6d3288bf67dfa8af40bb8
Nicholas Marques <nnmarques97@gmail.com>
2022-02-23 16:19:48 -0300

    test: add mockRequest and DonationInProcess integration tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -4,3 +4,11 @@
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
 import "jest-canvas-mock";
+
+export const mockNavigationFunction = jest.fn();
+jest.mock("hooks/useNavigation", () => ({
+  __esModule: true,
+  default: () => ({
+    navigateTo: mockNavigationFunction,
+  }),
+}));
