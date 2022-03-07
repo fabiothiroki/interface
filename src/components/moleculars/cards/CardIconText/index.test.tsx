63b5538a2f5f813073462cbd0f5ed89f34c700bc
Nicholas Marques <nnmarques97@gmail.com>
2022-03-07 14:17:02 -0300

    test: add CardIconText tests of right component render

--- src/components/moleculars/cards/CardIconText/index.test.tsx
+++ src/components/moleculars/cards/CardIconText/index.test.tsx
@@ -2,9 +2,19 @@ import { expectTextToBeInTheDocument, renderComponent } from "config/testUtils";
 import CardIconText from ".";
 
 describe("CardIconText", () => {
-  it("should render without error", () => {
+  it("renders the passed text", () => {
     renderComponent(<CardIconText text="CardIconText" icon="" />);
 
     expectTextToBeInTheDocument("CardIconText");
+  });
+
+  describe("when a right component is passed", () => {
+    it("renders the right component", () => {
+      renderComponent(
+        <CardIconText text="text" icon="" rightComponent={<div>test</div>} />,
+      );
+
+      expectTextToBeInTheDocument("test");
+    });
   });
 });
