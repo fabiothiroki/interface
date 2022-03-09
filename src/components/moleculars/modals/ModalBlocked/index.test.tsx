0eb23aa25508ab02156809bff421d36ff8ecf1d5
Nathiely Macedo <nathielymm@gmail.com>
2022-03-09 16:41:06 -0300

    fix: fix title modal blocked test

--- src/components/moleculars/modals/ModalBlocked/index.test.tsx
+++ src/components/moleculars/modals/ModalBlocked/index.test.tsx
@@ -3,8 +3,8 @@ import ModalBlocked from ".";
 
 describe("ModalBlocked", () => {
   it("should render without error", () => {
-    renderComponent(<ModalBlocked />);
+    renderComponent(<ModalBlocked title="ModalBlocked" visible />);
 
-    expectTextToBeInTheDocument("ModalBlocked");
+    expectTextToBeInTheDocument("");
   });
 });
