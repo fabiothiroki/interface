57c892ddd6aa84c7a720d4dd4222942d1ac649da
Clara Mendes <claraplanejamento@gmail.com>
2022-03-11 15:25:38 -0300

    feat: adds empty button navigation

--- src/components/moleculars/cards/CardEmptySection/index.tsx
+++ src/components/moleculars/cards/CardEmptySection/index.tsx
@@ -1,3 +1,4 @@
+import useNavigation from "hooks/useNavigation";
 import { ReactComponent as EmptyIcon } from "./assets/empty-section.svg";
 import * as S from "./styles";
 
@@ -6,11 +7,16 @@ export type Props = {
   btnText: string;
 };
 function CardEmptySection({ cardText, btnText }: Props): JSX.Element {
+  const { navigateTo } = useNavigation();
+  const handleButtonClick = () => {
+    navigateTo("/");
+  };
+
   return (
     <S.Container>
       <EmptyIcon width="50px" height="50px" />
       <S.Text>{cardText}</S.Text>
-      <S.EmptyDonationButton text={btnText} onClick={() => {}} />
+      <S.EmptyDonationButton text={btnText} onClick={handleButtonClick} />
     </S.Container>
   );
 }
