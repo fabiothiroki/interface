56097940e5dfe438eb8688f6e296675f9648ab8e
Nicholas Marques <nnmarques97@gmail.com>
2022-03-24 11:21:16 -0300

    fix: add undefined verification to currentUser lib

--- src/lib/currentUser/index.ts
+++ src/lib/currentUser/index.ts
@@ -4,7 +4,7 @@ import { CURRENT_USER_KEY } from "contexts/currentUserContext";
 
 export function currentUserFromStorage(): User | null {
   const userFromStorage = getLocalStorageItem(CURRENT_USER_KEY);
-  if (!userFromStorage) return null;
+  if (!userFromStorage || userFromStorage === "undefined") return null;
 
   return JSON.parse(userFromStorage);
 }
