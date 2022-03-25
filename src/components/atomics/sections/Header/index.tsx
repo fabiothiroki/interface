ab68d1b08719960ad8d237579504e42532a4ef49
Nathiely Macedo <nathielymm@gmail.com>
2022-03-25 11:48:43 -0300

    fix: remove useless props

--- src/components/atomics/sections/Header/index.tsx
+++ src/components/atomics/sections/Header/index.tsx
@@ -4,13 +4,8 @@ import * as S from "./styles";
 export type Props = {
   sideLogo?: string;
   rightComponent?: JSX.Element;
-  rightComponent2?: JSX.Element;
 };
-function Header({
-  sideLogo,
-  rightComponent,
-  rightComponent2,
-}: Props): JSX.Element {
+function Header({ sideLogo, rightComponent }: Props): JSX.Element {
   return (
     <S.Container>
       <S.LeftContainer>
@@ -18,12 +13,7 @@ function Header({
         <S.Divider>|</S.Divider>
         <S.Logo src={sideLogo} alt="side-logo" />
       </S.LeftContainer>
-      {rightComponent && (
-        <S.RightContainer>
-          {rightComponent2}
-          {rightComponent}
-        </S.RightContainer>
-      )}
+      {rightComponent && <S.RightContainer>{rightComponent}</S.RightContainer>}
     </S.Container>
   );
 }
