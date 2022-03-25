c1b98b14b54c2afe4d311a6e8d816e39e2d74761
Nicholas Marques <nnmarques97@gmail.com>
2022-03-25 08:28:41 -0300

    feat: add CardDoubleText

---
+++ src/components/moleculars/cards/CardDoubleText/index.stories.tsx
@@ -0,0 +1,26 @@
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import GlobeIcon from "assets/icons/globe-icon.svg";
+import Button from "components/atomics/Button";
+import CardDoubleText, { Props } from ".";
+
+export default {
+  title: "CardDoubleText",
+  component: CardDoubleText,
+} as ComponentMeta<typeof CardDoubleText>;
+
+const Template: ComponentStory<typeof CardDoubleText> = function (args: Props) {
+  return (
+    <div style={{ width: "328px" }}>
+      <CardDoubleText {...args} />
+    </div>
+  );
+};
+
+export const Default = Template.bind({});
+
+Default.args = {
+  title: "15/01/12",
+  subtitle: "12 alunos",
+  icon: GlobeIcon,
+  rightComponent: <Button text="text" onClick={() => {}} />,
+};
