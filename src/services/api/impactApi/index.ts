992d6bd525bb2c4e9558d9fdac564f340646ec8a
Clara Mendes <claraplanejamento@gmail.com>
2022-04-13 17:44:20 -0300

    fix: removes obsolete comment

--- src/services/api/impactApi/index.ts
+++ src/services/api/impactApi/index.ts
@@ -3,8 +3,6 @@ import Impact from "types/entities/Impact";
 import { apiGet } from "..";
 
 const impactApi = {
-  // FIXME: CHANGE THIS "ANY" RETURN TYPE ON AXIOS_RESPONSE!
-  // For example: if you're getting a list of feeds you can have a AxiosResponse<Feed[]>
   getImpact: (id: number | null): Promise<AxiosResponse<Impact[]>> =>
     apiGet(`users/${id}/impacts`),
 };
