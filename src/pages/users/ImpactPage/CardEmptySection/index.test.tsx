b50a39ee013c5a4cf7eab907d747d8385be6affc
Nicholas Marques <nnmarques97@gmail.com>
2022-04-28 11:37:29 -0300

    refactor: update CardEmptySection import on test

--- src/pages/users/ImpactPage/CardEmptySection/index.test.tsx
+++ src/pages/users/ImpactPage/CardEmptySection/index.test.tsx
@@ -1,5 +1,5 @@
 import { expectTextToBeInTheDocument, renderComponent } from "config/testUtils";
-import CardEmptySection from "./index";
+import CardEmptySection from ".";
 
 describe("CardEmptySection", () => {
   it("should render card text", () => {
