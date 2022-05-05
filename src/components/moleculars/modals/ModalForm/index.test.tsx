2a387557d5ed8fe0d97a7e9a82ef4e78768fb58d
Nathiely Macedo <nathielymm@gmail.com>
2022-05-05 15:30:34 -0300

    feat: add tests to modalForm

--- src/components/moleculars/modals/ModalForm/index.test.tsx
+++ src/components/moleculars/modals/ModalForm/index.test.tsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { expectTextToBeInTheDocument, renderComponent } from "config/testUtils";
+import {
+  expectTextToBeInTheDocument,
+  renderComponent,
+  clickOn,
+  expectTextNotToBeInTheDocument,
+} from "config/testUtils";
 import ModalForm from ".";
 
 describe("ModalForm", () => {
@@ -23,5 +28,40 @@ describe("ModalForm", () => {
     );
 
     expectTextToBeInTheDocument("ModalForm");
+  });
+
+  it("should handle on submit", () => {
+    const handleOnSubmitMock = jest.fn();
+    renderComponent(
+      <ModalForm
+        title="ModalForm"
+        visible
+        formFields={[
+          {
+            name: "email",
+            id: "email",
+            type: "email",
+            placeholder: "email",
+            required: true,
+          },
+        ]}
+        onFormSubmit={handleOnSubmitMock}
+        primaryButtonText="click"
+        initialState={{ email: "email" }}
+      />,
+    );
+    clickOn("click");
+    expect(handleOnSubmitMock).toHaveBeenCalled();
+  });
+
+  it("component is not visible and don't have title", () => {
+    renderComponent(
+      <ModalForm
+        formFields={[]}
+        onFormSubmit={() => {}}
+        initialState={{ email: "email" }}
+      />,
+    );
+    expectTextNotToBeInTheDocument("ModalForm");
   });
 });
