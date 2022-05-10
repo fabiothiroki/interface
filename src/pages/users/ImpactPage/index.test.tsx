6f78d11c1f4f4ca2230d9225bfe4f84d2a52e95f
Nicholas Marques <nnmarques97@gmail.com>
2022-05-10 15:25:23 -0300

    refactor: add logic to hide Show more button in ImpactPage

--- src/pages/users/ImpactPage/index.test.tsx
+++ src/pages/users/ImpactPage/index.test.tsx
@@ -1,4 +1,13 @@
-import { expectTextToBeInTheDocument, renderComponent } from "config/testUtils";
+import {
+  expectTextNotToBeInTheDocument,
+  expectTextToBeInTheDocument,
+  renderComponent,
+  waitForPromises,
+} from "config/testUtils";
+import impactFactory from "config/testUtils/factories/impactFactory";
+import { mockRequest } from "config/testUtils/test-helper";
+import userFactory from "config/testUtils/factories/userFactory";
+import nonProfitFactory from "config/testUtils/factories/nonProfitFactory";
 import Impact from ".";
 
 describe("Impact", () => {
@@ -16,6 +25,51 @@ describe("Impact", () => {
       expectTextToBeInTheDocument(
         "Your donation impact summary will be shown here",
       );
+    });
+  });
+
+  const user = userFactory({ id: 1 });
+
+  describe("when there are more cards to show", () => {
+    beforeEach(async () => {
+      renderComponent(<Impact />, {
+        currentUserProviderValue: {
+          currentUser: user,
+        },
+      });
+      mockRequest(`api/v1/users/${user.id}/impacts`, {
+        payload: [
+          impactFactory({ nonProfit: nonProfitFactory({ id: 1 }) }),
+          impactFactory({ nonProfit: nonProfitFactory({ id: 2 }) }),
+          impactFactory({ nonProfit: nonProfitFactory({ id: 3 }) }),
+        ],
+      });
+      await waitForPromises();
+    });
+
+    it("shows the show more button", async () => {
+      expectTextToBeInTheDocument("Show more");
+    });
+  });
+
+  describe("when there are no more cards to show", () => {
+    beforeEach(async () => {
+      renderComponent(<Impact />, {
+        currentUserProviderValue: {
+          currentUser: user,
+        },
+      });
+      mockRequest(`api/v1/users/${user.id}/impacts`, {
+        payload: [
+          impactFactory({ nonProfit: nonProfitFactory({ id: 1 }) }),
+          impactFactory({ nonProfit: nonProfitFactory({ id: 2 }) }),
+        ],
+      });
+      await waitForPromises();
+    });
+
+    it("shows the show more button", async () => {
+      expectTextNotToBeInTheDocument("Show more");
     });
   });
 });
