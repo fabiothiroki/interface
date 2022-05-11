9a0170c08e99679dcd866c7c5d72d5f7a23c69d5
Nicholas Marques <nnmarques97@gmail.com>
2022-05-11 15:29:46 -0300

    test: add tests to DonationTicketModal

---
+++ src/pages/donations/CausesPage/DonationTicketModal/index.test.tsx
@@ -0,0 +1,33 @@
+import {
+  expectTextNotToBeInTheDocument,
+  expectTextToBeInTheDocument,
+  renderComponent,
+} from "config/testUtils";
+import { DONATION_MODAL_SEEN_AT_KEY } from "lib/localStorage/constants";
+import DonationTicketModal from ".";
+
+describe("DonationTicketModal", () => {
+  describe("when the person has not seen the modal", () => {
+    beforeEach(() => {
+      localStorage.removeItem(DONATION_MODAL_SEEN_AT_KEY);
+      renderComponent(<DonationTicketModal />);
+    });
+
+    it("should be visible", () => {
+      expectTextToBeInTheDocument("You got a donation ticket");
+    });
+
+    it("sets the localStorage key correctly", () => {
+      expect(localStorage.getItem(DONATION_MODAL_SEEN_AT_KEY)).toBeTruthy();
+    });
+  });
+
+  describe("when the person has already seen the modal", () => {
+    it("should not be visible", () => {
+      localStorage.setItem(DONATION_MODAL_SEEN_AT_KEY, Date.now().toString());
+      renderComponent(<DonationTicketModal />);
+
+      expectTextNotToBeInTheDocument("You got a donation ticket");
+    });
+  });
+});
