5513a20d46a30398161bb6c4708ec1df1a629cef
Nicholas Marques <nnmarques97@gmail.com>
2022-05-16 09:29:11 -0300

    test: add Dropdown main features tests

--- src/components/atomics/Dropdown/index.test.tsx
+++ src/components/atomics/Dropdown/index.test.tsx
@@ -1,12 +1,38 @@
-import { expectTextToBeInTheDocument, renderComponent } from "config/testUtils";
+import {
+  clickOn,
+  expectTextToBeInTheDocument,
+  renderComponent,
+} from "config/testUtils";
 import Dropdown from ".";
 
 describe("Dropdown", () => {
-  it("should render without error", () => {
-    renderComponent(
-      <Dropdown name="dropdown" values={["test1", "test2"]} label="dropdown" />,
-    );
+  const mockFn = jest.fn();
 
-    expectTextToBeInTheDocument("Dropdown");
+  beforeEach(() => {
+    renderComponent(
+      <Dropdown
+        name="dropdown"
+        values={["test1", "test2"]}
+        label="dropdown"
+        onOptionChanged={mockFn}
+      />,
+    );
+  });
+
+  it("renders the label", () => {
+    expectTextToBeInTheDocument("dropdown");
+  });
+
+  it("shows the options when clicked", () => {
+    clickOn("dropdown");
+    expectTextToBeInTheDocument("test1");
+    expectTextToBeInTheDocument("test2");
+  });
+
+  it("calls the onOptionChanged function with correct params", () => {
+    clickOn("dropdown");
+    clickOn("test1");
+
+    expect(mockFn).toHaveBeenCalledWith("test1");
   });
 });
