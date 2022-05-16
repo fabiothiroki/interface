3bf9a6988298dfcd036caa269df2674e10627ffa
Nicholas Marques <nnmarques97@gmail.com>
2022-05-16 11:35:28 -0300

    refactor: add CardSection translation texts

--- src/pages/promoters/SupportFundPage/CardSection/index.tsx
+++ src/pages/promoters/SupportFundPage/CardSection/index.tsx
@@ -2,28 +2,35 @@ import { useTranslation } from "react-i18next";
 import { useState } from "react";
 import useGivingValues from "hooks/apiHooks/useGivingValues";
 import { useLanguage } from "hooks/useLanguage";
+import Dropdown from "components/atomics/Dropdown";
+import { Currencies } from "types/enums/Currencies";
 import * as S from "../styles";
 
 function CardSection(): JSX.Element {
   const [selectedButtonIndex, setSelectedButtonIndex] = useState(0);
 
   const { t } = useTranslation("translation", {
-    keyPrefix: "promoters.supportFundPage",
+    keyPrefix: "promoters.supportFundPage.cardSection",
   });
 
   const { currentLang } = useLanguage();
 
   function currentCoin() {
     if (currentLang === "pt-BR") {
-      return "brl";
+      return Currencies.BRL;
     }
-    return "usd";
+    return Currencies.USD;
   }
 
   const { givingValues } = useGivingValues(currentCoin());
 
   return (
     <div>
+      <Dropdown
+        name="currency"
+        label={t("currency")}
+        values={[Currencies.USD, Currencies.BRL]}
+      />
       <S.Subtitle>{t("subtitleCard")}</S.Subtitle>
 
       <S.ValuesContainer>
---
+++ src/types/enums/Currencies.ts
@@ -0,0 +1,7 @@
+// eslint-disable-next-line no-shadow
+export enum Currencies {
+  // eslint-disable-next-line no-unused-vars
+  USD = "usd",
+  // eslint-disable-next-line no-unused-vars
+  BRL = "brl",
+}
