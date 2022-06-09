cecb00070a74f9cf45f8f51f4638b15c8519c765
let <let@MacBook-Air-de-let.local>
2022-06-09 13:04:13 -0300

    test: add more tests to modal rows and remove unecessary style

--- src/components/moleculars/modals/ModalRows/index.test.tsx
+++ src/components/moleculars/modals/ModalRows/index.test.tsx
@@ -1,9 +1,19 @@
 import React from "react";
-import { renderComponent } from "config/testUtils";
+import { clickOn, renderComponent } from "config/testUtils";
 import icon from "assets/icons/ribon.svg";
-import { expectTextToBeInTheDocument } from "config/testUtils/expects";
+import {
+  expectTextToBeInTheDocument,
+  expectTextNotToBeInTheDocument,
+} from "config/testUtils/expects";
 import ModalRows from ".";
 
+jest.mock(
+  "../../../atomics/LottieAnimation",
+  () =>
+    function () {
+      return <p>animation</p>;
+    },
+);
 describe("ModalRows", () => {
   it("should render without error", () => {
     renderComponent(
@@ -20,5 +30,116 @@ describe("ModalRows", () => {
     );
 
     expectTextToBeInTheDocument("rows");
+  });
+
+  it("should not show modal when visible is false", () => {
+    renderComponent(
+      <ModalRows
+        rowsContent={[
+          {
+            id: 1,
+            icon,
+            text: "rows",
+          },
+        ]}
+      />,
+    );
+
+    expectTextNotToBeInTheDocument("rows");
+  });
+
+  it("should render animation", () => {
+    const animationData = jest.fn();
+    renderComponent(<ModalRows visible animationData={animationData} />);
+
+    expectTextToBeInTheDocument("animation");
+  });
+
+  describe("Buttons", () => {
+    it("should call function when click primary", () => {
+      const handlePrimaryButton = jest.fn();
+      renderComponent(
+        <ModalRows
+          rowsContent={[
+            {
+              id: 1,
+              icon,
+              text: "rows",
+            },
+          ]}
+          visible
+          primaryButtonText="click"
+          primaryButtonCallback={handlePrimaryButton}
+        />,
+      );
+
+      expectTextToBeInTheDocument("click");
+      clickOn("click");
+      expect(handlePrimaryButton).toHaveBeenCalled();
+    });
+
+    it("should not call function when click primary button", () => {
+      const handlePrimaryButton = jest.fn();
+      renderComponent(
+        <ModalRows
+          rowsContent={[
+            {
+              id: 1,
+              icon,
+              text: "rows",
+            },
+          ]}
+          visible
+          primaryButtonText="click"
+        />,
+      );
+
+      expectTextToBeInTheDocument("click");
+      clickOn("click");
+      expect(handlePrimaryButton).not.toHaveBeenCalled();
+    });
+
+    it("should call function when click secondary button", () => {
+      const handleSecondaryButton = jest.fn();
+      renderComponent(
+        <ModalRows
+          rowsContent={[
+            {
+              id: 1,
+              icon,
+              text: "rows",
+            },
+          ]}
+          visible
+          secondaryButtonText="click"
+          secondaryButtonCallback={handleSecondaryButton}
+        />,
+      );
+
+      expectTextToBeInTheDocument("click");
+      clickOn("click");
+      expect(handleSecondaryButton).toHaveBeenCalled();
+    });
+
+    it("should not call function when click secondary button", () => {
+      const handleSecondaryButton = jest.fn();
+      renderComponent(
+        <ModalRows
+          rowsContent={[
+            {
+              id: 1,
+              icon,
+              text: "rows",
+            },
+          ]}
+          visible
+          secondaryButtonText="click"
+        />,
+      );
+
+      expectTextToBeInTheDocument("click");
+      clickOn("click");
+      expect(handleSecondaryButton).not.toHaveBeenCalled();
+    });
   });
 });
