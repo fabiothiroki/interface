1fe29ab4891c6dc0817a19be88a7ea49ed94653a
Juju <juju@ribon.io>
2022-06-10 10:42:04 -0300

    fix: networks and update contracts

--- src/config/networks/index.ts
+++ src/config/networks/index.ts
@@ -1,24 +1,24 @@
-export const networks = {
-  mumbai: {
+export const networks = [
+  {
+    chainName: "Mumbai Testnet",
     ribonContractAddress: "0x38D30f5123e774E26D60C02d4B2927b90953E3d5",
     donationTokenContractAddress: "0x21A72dc641c8e5f13717a7e087d6D63B4f9A3574",
     chainId: 0x13881,
-    chainName: "Mumbai Testnet",
     rpcUrls: "https://rpc-mumbai.maticvigil.com",
     nodeUrl: "https://polygon-mumbai.g.alchemy.com/v2/1fEWpdSHuohPveNBGvlozE6qv9P1uAks",
     symbolName: "https://mumbai.polygonscan.com/",
     currencyName: "MATIC",
     blockExplorerUrls: "Matic"
   },
-  localhost: {
+  {
+    chainName: "Localhost 8545",
     ribonContractAddress: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
     donationTokenContractAddress: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
     chainId: 0x539,
-    chainName: "Localhost 8545",
     rpcUrls: "http://localhost:8545",
     nodeUrl: "http://localhost:8545",
     symbolName: "ETH",
     currencyName: "Ether",
     blockExplorerUrls: "http://localhost:8545",
   }
-}
+];
--- src/hooks/useNetwork/index.ts
+++ src/hooks/useNetwork/index.ts
@@ -3,7 +3,7 @@ import { networks } from "config/networks";
 import { useProvider } from "../useProvider";
 
 export function useNetwork() {
-  const [currentNetwork, setCurrentNetwork] = useState(networks.localhost);
+  const [currentNetwork, setCurrentNetwork] = useState(networks[0]);
   const [isValidNetwork, setIsValidNetwork] = useState(false);
   const provider = useProvider();
 
@@ -11,19 +11,34 @@ export function useNetwork() {
     try {
       const providerNetwork = await provider?.getNetwork()
       if (providerNetwork) {
-        setIsValidNetwork(false);
-        Object.entries(networks).map((network) => {
-          if (providerNetwork.chainId === network[1].chainId) {
-            setCurrentNetwork(network[1]);
-            setIsValidNetwork(true);
-          }
-          return null;
-        });
+        const permittedNetworks = networks.filter((network) => providerNetwork.chainId === network.chainId);
+        if (permittedNetworks.length > 0) {
+          setCurrentNetwork(permittedNetworks[0]);
+          setIsValidNetwork(true);
+        }
       }
     } catch (e) {
       console.log(e);
     }
   }, [provider]);
+
+  const changeNetwork = () => {
+    const defaultNetwork = networks[0];
+    window.ethereum.request({
+      method: "wallet_addEthereumChain",
+      params: [{
+          chainId: defaultNetwork.chainId,
+          rpcUrls: [defaultNetwork.rpcUrls],
+          chainName: defaultNetwork.chainName,
+          nativeCurrency: {
+              name: defaultNetwork.currencyName,
+              symbol: defaultNetwork.symbolName,
+              decimals: 18
+          },
+          blockExplorerUrls: [defaultNetwork.blockExplorerUrls]
+      }]
+  });
+  }
 
   useEffect(() => {
     getCurrentNetwork();
@@ -36,5 +51,6 @@ export function useNetwork() {
   return {
     currentNetwork,
     isValidNetwork,
+    changeNetwork,
   };
 }
