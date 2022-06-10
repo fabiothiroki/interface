c8de10ffa382a13f4a7b03d093d88658ef194e59
Juju <juju@ribon.io>
2022-06-10 09:51:47 -0300

    fix: networks and update contracts

--- src/config/networks/index.ts
+++ src/config/networks/index.ts
@@ -1,11 +1,11 @@
 export const networks = {
   mumbai: {
-    ribonContractAddress: "0x7aB7Aaf626D639B0f9EcFdF4A01E51F75E2eB596",
+    ribonContractAddress: "0x38D30f5123e774E26D60C02d4B2927b90953E3d5",
     donationTokenContractAddress: "0x21A72dc641c8e5f13717a7e087d6D63B4f9A3574",
     chainId: 0x13881,
     chainName: "Mumbai Testnet",
-    rpcUrls: "https://polygon-mumbai.g.alchemy.com/v2/1fEWpdSHuohPveNBGvlozE6qv9P1uAks",
-    nodeUrl: "https://rpc-mumbai.maticvigil.com",
+    rpcUrls: "https://rpc-mumbai.maticvigil.com",
+    nodeUrl: "https://polygon-mumbai.g.alchemy.com/v2/1fEWpdSHuohPveNBGvlozE6qv9P1uAks",
     symbolName: "https://mumbai.polygonscan.com/",
     currencyName: "MATIC",
     blockExplorerUrls: "Matic"
