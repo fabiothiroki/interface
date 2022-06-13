8b136a50b494907c4949d51fe6ec31d5dffc36a6
Henrique Barbosa <henrikleod@gmail.com>
2022-06-13 10:57:38 -0300

    test: add remaining tests on useUsers hook

--- src/hooks/apiHooks/useUsers/index.test.ts
+++ src/hooks/apiHooks/useUsers/index.test.ts
@@ -7,10 +7,13 @@ describe("useUsers", () => {
   const testEmail = "test@email.com";
   const data = { name: "test user" };
 
+  beforeEach(() => {
+    const { result } = renderHook(() => useUsers());
+    hook = result.current;
+  });
+  
   describe("#findUser", () => {
     beforeEach(() => {
-      const { result } = renderHook(() => useUsers());
-      hook = result.current;
       usersApi.postSearchUser = jest.fn(() => ({ data } as any));
     });
 
@@ -23,6 +26,70 @@ describe("useUsers", () => {
     it("returns the data fetched from the api", async () => {
       const findResultResult = await hook.findUser(testEmail);
       expect(findResultResult).toEqual(data);
+    });
+  });
+
+  describe("#createUser", () => {
+    beforeEach(() => {
+      usersApi.postCreateUser = jest.fn(() => ({ data } as any));
+    });
+
+    it("calls the usersApi createUser with correct params", () => {
+      hook.createUser(testEmail);
+
+      expect(usersApi.postCreateUser).toHaveBeenCalledWith(testEmail);
+    });
+
+    it("returns the data fetched from the api", async () => {
+      const findResultResult = await hook.createUser(testEmail);
+      expect(findResultResult).toEqual(data);
+    });
+  });
+
+  describe("#findOrCreateUser", () => {
+    describe("when user exixts on the database", () => {
+      beforeEach(() => {
+        usersApi.postSearchUser = jest.fn(() => ({ data } as any));
+      });
+  
+      it("calls the usersApi searchUser with correct params", () => {
+        hook.findOrCreateUser(testEmail);
+  
+        expect(usersApi.postSearchUser).toHaveBeenCalledWith(testEmail);
+      });
+  
+      it("returns the data fetched from the api", async () => {
+        const findResultResult = await hook.findOrCreateUser(testEmail);
+        expect(findResultResult).toEqual(data);
+      });
+    });
+
+    describe("when user don't exists on the database", () => {
+      const createdUser = { name: "newly created user" }
+
+      beforeEach(() => {
+        usersApi.postSearchUser = jest.fn(() => { throw new Error() });
+        usersApi.postCreateUser = jest.fn(() => ({ data: createdUser } as any));
+      });
+  
+      it("calls the usersApi searchUser with correct params and throws an error", () => {
+        hook.findOrCreateUser(testEmail);
+  
+        expect(usersApi.postSearchUser).toHaveBeenCalledWith(testEmail);
+        expect(usersApi.postSearchUser).toThrow();
+      });
+
+      it("calls the usersApi createUser with correct params", () => {
+        hook.createUser(testEmail);
+
+        expect(usersApi.postCreateUser).toHaveBeenCalledWith(testEmail);
+      });
+
+
+      it("creates an user and returns the data", async () => {
+        const findResultResult = await hook.findOrCreateUser(testEmail);
+        expect(findResultResult).toEqual(createdUser);
+      });
     });
   });
 });
