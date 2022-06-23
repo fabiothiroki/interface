71450583445ffeeca7d258fa35c4fe229074561e
Nicholas Marques <nnmarques97@gmail.com>
2022-06-23 11:26:55 -0300

    refactor: add walletTruncate to WalletLayout and add more tests to it

--- src/layouts/WalletLayout/index.test.tsx
+++ src/layouts/WalletLayout/index.test.tsx
@@ -4,16 +4,76 @@ import {
   expectTextToBeInTheDocument,
 } from "config/testUtils/expects";
 import WalletLayout from ".";
+import { clickOn } from "../../config/testUtils";
 
 describe("WalletLayout", () => {
   it("renders the children passed", () => {
     renderComponent(
-      <WalletLayout hideNavigation>
+      <WalletLayout>
         <div>test</div>
       </WalletLayout>,
     );
 
     expectTextToBeInTheDocument("test");
+  });
+
+  it("shows the navigation", () => {
+    renderComponent(
+      <WalletLayout>
+        <div>test</div>
+      </WalletLayout>,
+    );
+
+    expectTextToBeInTheDocument("Treasure");
+    expectTextToBeInTheDocument("Causes");
+    expectTextToBeInTheDocument("Impact");
+  });
+
+  describe("when the wallet is connected", () => {
+    beforeEach(() => {
+      renderComponent(
+        <WalletLayout hideNavigation>
+          <div>test</div>
+        </WalletLayout>,
+        {
+          walletProviderValue: {
+            wallet: "0x6E060041D62fDd76cF27c582f62983b864878E8F",
+          },
+        },
+      );
+    });
+
+    it("shows the truncated wallet", () => {
+      expectTextToBeInTheDocument("0x6E...8E8F");
+    });
+  });
+
+  describe("when the wallet is not connected", () => {
+    const mockConnectWallet = jest.fn();
+
+    beforeEach(() => {
+      renderComponent(
+        <WalletLayout hideNavigation>
+          <div>test</div>
+        </WalletLayout>,
+        {
+          walletProviderValue: {
+            connectWallet: mockConnectWallet,
+          },
+        },
+      );
+    });
+
+    it("shows the connect wallet button", () => {
+      expectTextToBeInTheDocument("Connect");
+    });
+
+    describe("when the connect button is clicked", () => {
+      it("calls the connect wallet method", () => {
+        clickOn("Connect");
+        expect(mockConnectWallet).toHaveBeenCalled();
+      });
+    });
   });
 
   describe("when hide navigation is passed", () => {
