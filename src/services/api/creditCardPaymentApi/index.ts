4d204f59844cab0e4bcd56a5ac6792ebad9a6b9a
let <let@MacBook-Air-de-let.local>
2022-06-28 13:47:17 -0300

    fix: adjust route credit card payment

--- src/services/api/creditCardPaymentApi/index.ts
+++ src/services/api/creditCardPaymentApi/index.ts
@@ -6,7 +6,7 @@ const creditCardPaymentApi = {
   postCreditCardPayment: (
     paymentInformations: CreditCardPayment,
   ): Promise<AxiosResponse<CreditCardPayment>> =>
-    apiPost("payments/credit_cards/subscribe", { paymentInformations }),
+    apiPost("payments/credit_cards", { paymentInformations }),
 };
 
 export default creditCardPaymentApi;
