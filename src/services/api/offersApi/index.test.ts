636fcf7d470b0b22985e1217a16707357876156e
Nicholas Marques <nnmarques97@gmail.com>
2022-06-28 13:24:11 -0300

    feat: add useOffers hook

--- src/services/api/offersApi/index.test.ts
+++ src/services/api/offersApi/index.test.ts
@@ -1,3 +1,4 @@
+import { Currencies } from "types/enums/Currencies";
 import offersApi from ".";
 import api from "..";
 
@@ -8,9 +9,11 @@ describe("offersApi", () => {
     });
 
     it("expects to send a get request with the correct info: url, params and headers", () => {
-      offersApi.getOffers();
+      offersApi.getOffers(Currencies.BRL, false);
 
-      expect(api.get).toHaveBeenCalledWith("/api/v1/givings/offers");
+      expect(api.get).toHaveBeenCalledWith(
+        "/api/v1/givings/offers?currency=brl&subscription=false",
+      );
     });
   });
 });
--- src/services/api/offersApi/index.ts
+++ src/services/api/offersApi/index.ts
@@ -1,9 +1,16 @@
 import { AxiosResponse } from "axios";
 import Offer from "types/entities/Offer";
+import { Currencies } from "types/enums/Currencies";
 import { apiGet } from "..";
 
 const offersApi = {
-  getOffers: (): Promise<AxiosResponse<Offer[]>> => apiGet("givings/offers"),
+  getOffers: (
+    currency: Currencies,
+    subscription: boolean,
+  ): Promise<AxiosResponse<Offer[]>> =>
+    apiGet(
+      `givings/offers?currency=${currency.toLowerCase()}&subscription=${subscription}`,
+    ),
 };
 
 export default offersApi;
