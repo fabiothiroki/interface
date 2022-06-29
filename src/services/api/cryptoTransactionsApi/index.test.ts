87071d5c1776f443930a2b64cacace41175c2601
Henrique Barbosa <henrikleod@gmail.com>
2022-06-29 14:39:55 -0300

    feature: crypto transacrion api tests

---
+++ src/services/api/cryptoTransactionsApi/index.test.ts
@@ -0,0 +1,38 @@
+import { TransactionStatus } from "types/enums/TransactionStatus";
+import cryptoTransactionApi from ".";
+import api from "..";
+
+describe("cryptoTransacrionApi", () => {
+  describe("#postTransacrion", () => {
+    beforeEach(() => {
+      api.post = jest.fn();
+    });
+
+    it("expects to send a get request with the correct info: url, params and headers", () => {
+      cryptoTransactionApi.postTransaction("0xAAAA", "0xBBBB", "0xCCCC", 100, TransactionStatus.PROCESSING);
+
+      expect(api.post).toHaveBeenCalledWith("/api/v1/payments/cryptocurrency", {
+        hash: "0xAAAA",
+        walletAddress: "0xBBBB",
+        contractAddress: "0xCCCC",
+        amount: 100,
+        status: TransactionStatus.PROCESSING
+      });
+    });
+  });
+
+  describe("#putTransacrion", () => {
+    beforeEach(() => {
+      api.put = jest.fn();
+    });
+
+    it("expects to send a get request with the correct info: url, params and headers", () => {
+      cryptoTransactionApi.putTransactionStatus("0xCCCC", TransactionStatus.SUCCESS);
+
+      expect(api.put).toHaveBeenCalledWith("/api/v1/payments/cryptocurrency", {
+        hash: "0xCCCC",
+        status: TransactionStatus.SUCCESS
+      });
+    });
+  });
+});
