25d5ad44c11b4594e4315025597faa53f417e9ce
Henrique Barbosa <henrikleod@gmail.com>
2022-06-29 14:42:40 -0300

    feature: transaction status enum

---
+++ src/types/enums/TransactionStatus.ts
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-shadow
+export enum TransactionStatus {
+  // eslint-disable-next-line no-unused-vars
+  PROCESSING = "processing",
+  // eslint-disable-next-line no-unused-vars
+  SUCCESS = "success",
+  // eslint-disable-next-line no-unused-vars
+  FAILED = "failed",
+}
