f45952e507106e6801252d8d82a41bd3d896687d
thaisribon <thais@ribon.io>
2022-07-28 18:36:25 -0300

    fix: exit button adjustments

--- src/layouts/LayoutHeader/LogoutItem/styles.ts
+++ src/layouts/LayoutHeader/LogoutItem/styles.ts
@@ -1,5 +1,15 @@
+import Button from "components/atomics/Button";
 import styled, { css } from "styled-components";
 
 export const Container = styled.div`
   ${() => css``}
+`;
+
+export const LogoutButton = styled(Button)`
+  ${() => css`
+    background: #ffffff;
+    padding: 4px 12px;
+    line-height: 16px;
+    min-width: 76px;
+  `}
 `;
