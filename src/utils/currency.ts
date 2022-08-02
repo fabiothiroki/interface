5fc9f73e757f20d2744e1f039aba98809cb5a339
thaisribon <thais@ribon.io>
2022-08-02 14:56:28 -0300

    fix: removeInsignificantZeros function

--- src/utils/currency.ts
+++ src/utils/currency.ts
@@ -1,3 +1,5 @@
 export function removeInsignificantZeros(price: string) {
-  return price.split(".00")[0];
+  const last3 = price.slice(price.length - 3);
+  if (last3 === ".00" || last3 === ",00") return price.split(last3)[0];
+  return price;
 }
