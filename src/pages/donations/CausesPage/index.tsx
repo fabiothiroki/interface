1dbd9a3f24a245dd169a869536fd7812a888d6d3
Nicholas Marques <nnmarques97@gmail.com>
2022-08-02 16:26:20 -0300

    feat: add donateDonationErro_view event

--- src/pages/donations/CausesPage/index.tsx
+++ src/pages/donations/CausesPage/index.tsx
@@ -75,6 +75,10 @@ function CausesPage(): JSX.Element {
     }
   }, []);
 
+  useEffect(() => {
+    if (state?.failedDonation) logEvent("donateDonationError_view");
+  }, []);
+
   const closeConfirmModal = useCallback(() => {
     setConfirmModalVisible(false);
   }, []);
