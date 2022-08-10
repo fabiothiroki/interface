d9ea87e06ce83efda056fe84c0a48f944821e63c
Clara Mendes <claraplanejamento@gmail.com>
2022-08-10 16:31:22 -0300

    feat: add event

--- src/hooks/modalHooks/useDonationTicketModal/index.tsx
+++ src/hooks/modalHooks/useDonationTicketModal/index.tsx
@@ -2,6 +2,7 @@ import { useTranslation } from "react-i18next";
 import { MODAL_TYPES } from "contexts/modalContext/helpers";
 import { useEffect } from "react";
 import Ticket from "assets/images/ticket.svg";
+import { logEvent } from "services/analytics";
 import { useModal } from "../useModal";
 
 export function useDonationTicketModal(initialState?: boolean) {
@@ -24,6 +25,7 @@ export function useDonationTicketModal(initialState?: boolean) {
 
   const showDonationTicketModal = () => {
     show();
+    logEvent("dailyTicketDial_view");
   };
 
   const hideDonationTicketModal = () => {
