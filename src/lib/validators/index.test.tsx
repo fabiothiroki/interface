0134c2767715baddd558b9e9cf351493aa42b7ed
Henrique Barbosa <henrikleod@gmail.com>
2022-08-22 13:21:35 -0300

    feature: add uuid validation

--- src/lib/validators/index.test.tsx
+++ src/lib/validators/index.test.tsx
@@ -1,4 +1,4 @@
-import { isValidEmail } from ".";
+import { isValidEmail, isValidUuid } from ".";
 
 describe("should check email", () => {
   it("expects to return false", () => {
@@ -9,3 +9,14 @@ describe("should check email", () => {
     expect(isValidEmail("foo@bar.com")).toBe(true);
   });
 });
+
+describe("should check uuid", () => {
+  it("expects to return false", () => {
+    expect(isValidUuid("invalid_uuid")).toBe(false);
+  });
+
+  it("expects to return true", () => {
+    expect(isValidUuid("c5c680cf-3a7a-4ea1-ade6-e5eb77e83c88")).toBe(true);
+  });
+});
+
