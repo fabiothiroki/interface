1bfe7b54571c9246c35f1fc4b6f85858c8e70c01
Henrique Barbosa <henrikleod@gmail.com>
2022-08-22 13:11:55 -0300

    feature: allow string in the api service

--- src/services/api/integrationsApi/index.test.ts
+++ src/services/api/integrationsApi/index.test.ts
@@ -7,10 +7,17 @@ describe("integrationsApi", () => {
       api.get = jest.fn();
     });
 
-    it("expects to send a get request with the correct info: url, params and headers", () => {
+    it("expects to send a get request with the correct info: url, id and headers", () => {
       integrationsApi.getIntegration(1);
 
       expect(api.get).toHaveBeenCalledWith("/api/v1/integrations/1");
+    });
+
+    it("expects to send a get request with the correct info: url, uuid and headers", () => {
+      const uuid = "64a3a5af-c249-4815-b89a-43986efb0de7"
+      integrationsApi.getIntegration(uuid);
+
+      expect(api.get).toHaveBeenCalledWith(`/api/v1/integrations/${uuid}`);
     });
   });
 });
