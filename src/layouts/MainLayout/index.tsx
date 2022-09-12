0363d22ded5491e2d2a5cd7a0855632f0126266f
thaisribon <thais@ribon.io>
2022-09-12 14:01:03 -0300

    feat: tests

--- src/layouts/MainLayout/index.tsx
+++ src/layouts/MainLayout/index.tsx
@@ -1,6 +1,8 @@
 import LayoutHeader from "layouts/LayoutHeader";
 import Navigation from "config/routes/Navigation";
 import { menuInFirstDonationFeature } from "config/abTest/features";
+import { getLocalStorageItem } from "lib/localStorage";
+import { useCurrentUser } from "contexts/currentUserContext";
 import * as S from "./styles";
 
 export type Props = {
@@ -8,8 +10,14 @@ export type Props = {
   hideHeader?: boolean;
 };
 function MainLayout({ children, hideHeader = false }: Props): JSX.Element {
+  const { signedIn } = useCurrentUser();
+
   function hasShowNavigationBar() {
-    if (menuInFirstDonationFeature()) {
+    if (
+      menuInFirstDonationFeature() ||
+      getLocalStorageItem("HAS_DONATED") === "true" ||
+      signedIn
+    ) {
       return true;
     }
     return false;
