8932f5de903ab55bfe6a12d1c2e48a3b4c98f457
Nicholas Marques <nnmarques97@gmail.com>
2022-09-12 14:26:07 -0300

    fix: add ribon api and ribon company env vars

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,7 +3,9 @@ export const RIBON_COMPANY_ID = process.env.REACT_APP_RIBON_COMPANY_ID || "1";
 export const RIBON_GROWTHBOOK_URL =
   "https://growthbook.ribon.io:444/api/features/key_prod_2161769c509d739b";
 
-export const RIBON_API = process.env.REACT_APP_RIBON_API || "http://ribon-core-api-dev.us-east-1.elasticbeanstalk.com/";
+export const RIBON_API =
+  process.env.REACT_APP_RIBON_API ||
+  "http://ribon-core-api-dev.us-east-1.elasticbeanstalk.com/";
 
 export const THE_GRAPH_API =
   "https://api.thegraph.com/subgraphs/name/ribondao/subgraphribon";
