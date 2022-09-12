2ccde7a1e9650d3c50729f5ab5c3315b0f75d226
Clara Mendes <claraplanejamento@gmail.com>
2022-09-12 08:23:49 -0300

    refactor: changes logout button to auxiliar button

--- src/layouts/LayoutHeader/LogoutItem/styles.ts
+++ src/layouts/LayoutHeader/LogoutItem/styles.ts
@@ -1,9 +1,9 @@
-import Button from "components/atomics/buttons/Button";
+import { AuxiliarButton } from "components/atomics/typography/AuxiliarButtonText/styles";
 import styled from "styled-components";
 
 export const Container = styled.div``;
 
-export const LogoutButton = styled(Button)`
+export const LogoutButton = styled(AuxiliarButton)`
   min-width: 76px;
   padding: 4px 12px;
   background: ${({ theme }) => theme.colors.white};
