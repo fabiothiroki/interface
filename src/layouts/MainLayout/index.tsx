e0261a12a20fc0866071c7fe271acf49ff30dac8
Nathiely Macedo <nathielymm@gmail.com>
2022-09-13 11:14:28 -0300

    fix: remove ab test function from condition

--- src/layouts/MainLayout/index.tsx
+++ src/layouts/MainLayout/index.tsx
@@ -1,6 +1,5 @@
 import LayoutHeader from "layouts/LayoutHeader";
 import Navigation from "config/routes/Navigation";
-import { menuInFirstDonationFeature } from "config/abTest/features";
 import { getLocalStorageItem } from "lib/localStorage";
 import { useCurrentUser, SHOW_MENU } from "contexts/currentUserContext";
 import * as S from "./styles";
@@ -13,11 +12,7 @@ function MainLayout({ children, hideHeader = false }: Props): JSX.Element {
   const { signedIn } = useCurrentUser();
 
   function hasShowNavigationBar() {
-    if (
-      menuInFirstDonationFeature() &&
-      signedIn &&
-      getLocalStorageItem(SHOW_MENU) === "true"
-    ) {
+    if (signedIn && getLocalStorageItem(SHOW_MENU) === "true") {
       return true;
     }
     return false;
