6ad6f20e9f2c6e142e01b3cb6d77de55c7870d71
Nathiely Macedo <nathielymm@gmail.com>
2022-09-13 10:28:56 -0300

    feat: add local storage key show_menu to tests

--- src/layouts/MainLayout/index.test.tsx
+++ src/layouts/MainLayout/index.test.tsx
@@ -1,5 +1,6 @@
 import { renderComponent } from "config/testUtils/renders";
 import { expectTextToBeInTheDocument } from "config/testUtils/expects";
+import { setLocalStorageItem } from "lib/localStorage";
 import MainLayout from ".";
 
 jest.mock(
@@ -22,6 +23,7 @@ describe("MainLayout", () => {
   });
 
   it("show navigation when user is logged", () => {
+    setLocalStorageItem("SHOW_MENU", "true");
     renderComponent(
       <MainLayout>
         <div>test</div>
