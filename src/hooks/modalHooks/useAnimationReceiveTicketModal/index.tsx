352eeb95f9f9d4aa450d458f4078a88405ec3ffe
let <let@MacBook-Air-de-let.local>
2022-09-19 11:03:16 -0300

    test: remove console.log

--- src/hooks/modalHooks/useAnimationReceiveTicketModal/index.tsx
+++ src/hooks/modalHooks/useAnimationReceiveTicketModal/index.tsx
@@ -27,8 +27,6 @@ export function useAnimationReceiveTicketModal(initialState?: boolean) {
   const showAnimationReceiveTicketModal = () => {
     show();
     setTimeout(() => {
-      // eslint-disable-next-line no-console
-      console.log(localStorage.getItem("integrationName"));
       logEvent("dailyTicketDial_view");
       hide();
     }, 3000);
